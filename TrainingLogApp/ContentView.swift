725d8827792394a921b94f0313e4cf6d5657ea7a
yichenzhong <yichenpsy@gmail.com>
2026-06-27 16:39:24 +0200

    Initial Commit

---
+++ TrainingLogApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrainingLogApp
+//
+//  Created by Yichen Zhong on 27.06.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrainingLogApp/TrainingLogAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingLogAppApp.swift
+//  TrainingLogApp
+//
+//  Created by Yichen Zhong on 27.06.26.
+//
+
+import SwiftUI
+
+@main
+struct TrainingLogAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
